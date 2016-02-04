862cde5dd506c5dd1b9834ba8a6d4f1ff761c375
Alex Morega <alex@grep.ro>
2016-02-04 15:40:35 +0200

    refactor build command

--- devel.js
+++ devel.js
@@ -1,6 +1,20 @@
 import webpack from 'webpack'
 import fs from 'fs'
 
+const WEBAPP_OPTIONS = {
+  entry: './src/app.js',
+  devtool: '#inline-source-map',
+  output: {path: 'build', filename: 'webapp.js'},
+  module: {
+    loaders: [
+      {test: /\.js$/, loader: 'babel', query: {presets: 'es2015,stage-0'}},
+    ],
+    noParse: [
+      /\/vendor\.js$/,
+    ],
+  },
+}
+
 function webpackBuild(options) {
   return new Promise((resolve, reject) => {
     webpack(options, function(err, stats) {
@@ -16,20 +30,22 @@ function buildIndex(options) {
   fs.writeFileSync('./build/index.html', indexHtml, 'utf8')
 }
 
-(async function() {
-  let stats = await webpackBuild({
-    entry: './src/app.js',
-    devtool: '#inline-source-map',
-    output: {path: 'build', filename: 'webapp.js'},
-    module: {
-      loaders: [
-        {test: /\.js$/, loader: 'babel', query: {presets: 'es2015,stage-0'}},
-      ],
-      noParse: [
-        /\/vendor\.js$/,
-      ],
-    },
-  })
+async function build() {
+  let stats = await webpackBuild(WEBAPP_OPTIONS)
   console.log(stats.toString({colors: true}))
   buildIndex({webappHash: stats.toJson().chunks[0].hash})
+}
+
+(async function() {
+  let cmd = process.argv[2]
+
+  switch(cmd) {
+
+    case 'build':
+      return await build()
+
+    default:
+      throw new Error("Unknown command " + cmd)
+
+  }
 })().catch((e) => { console.error(e) })
