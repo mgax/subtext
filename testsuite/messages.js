78895a80862e376c857c9f43f0e7bd7021088e1b
Alex Morega <alex@grep.ro>
2016-02-04 13:12:34 +0200

    a test with fixtures for keys and message

--- testsuite/messages.js
+++ testsuite/messages.js
@@ -66,4 +66,23 @@ describe('identity', function() {
     assert.deepEqual(payload, {hello: 'world'})
   })
 
+  it('should read messages in standard format', function() {
+
+    let alicePublicKey = {
+      type: 'publickey',
+      key: 'j/f5Is/1wuq/Md/NDX9F/4cmpHaiAgaoiILb9pWcuig=',
+    }
+    let bobPrivateKey = {
+      type: 'privatekey',
+      key: '4ROZBrZFj1yedGogzIvI0Q4ZTDnvdbh9WvaQkozTW7M=',
+    }
+    let cryptobox = {
+      type: 'cryptobox',
+      ciphertext: 'aPxoDYldHBo4HlJQaAAj1G9mdiiEv9JyQ0KzRxQdza4V',
+      nonce: '4thKliPbaeA3LM1r/YLiRo7xjZXgSs9j',
+    }
+    let payload = open_box(cryptobox, bobPrivateKey, alicePublicKey)
+    assert.deepEqual(payload, {hello: 'world'})
+  })
+
 })
