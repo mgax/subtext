2f4ed321d28b888ab8d78f590c03621e84f4dfc4
Alex Morega <alex@grep.ro>
2016-02-21 20:57:18 +0200

    decrypt incoming message

--- src/identityserver.js
+++ src/identityserver.js
@@ -1,22 +1,31 @@
 import fs from 'fs'
 import express from 'express'
 import bodyParser from 'body-parser'
+import { openBox } from './messages.js'
 
 export default function(identityPath) {
   let config = JSON.parse(fs.readFileSync(identityPath + '/config.json'))
+  let { keyPair, messageUrl } = config
+
+  function receive({ box, from, to }) {
+    if(to.key != keyPair.publicKey.key) return {error: "Message is not for me"}
+    console.log(openBox(box, keyPair.privateKey, from))
+    return {ok: true}
+  }
+
   let app = express()
   app.use(bodyParser.json())
 
   app.get('/finger', (req, res) => {
     res.send({
-      publicKey: config.keyPair.publicKey,
-      messageUrl: config.messageUrl,
+      publicKey: keyPair.publicKey,
+      messageUrl: messageUrl,
     })
   })
 
   app.post('/message', (req, res) => {
-    console.log(req.body)
-    res.send({ok: true})
+    let result = receive(req.body)
+    res.send(result)
   })
 
   return app
