072e7e4db1d4698081a70b992f637d00edbdd670
Alex Morega <alex@grep.ro>
2016-03-27 18:12:26 +0300

    extract common testing stuff

---
+++ testsuite/common.js
@@ -0,0 +1,105 @@
+import tmp from 'tmp'
+import fs from 'fs'
+import request from 'supertest'
+import { createBox } from '../src/messages.js'
+
+export const ALICE = {
+  keyPair: {
+    type: "KeyPair",
+    publicKey: {
+      type: "PublicKey",
+      key: "YRgaMPzdZPAQiWFiiCggx5qppkN5LNsFTvuoXFF5kDA=",
+    },
+    privateKey: {
+      type: "PrivateKey",
+      key: "yjwWdAYTsOrUW12amKnT1J1R6TIu0FmjZ+JXCmzyW4A=",
+    },
+  },
+  name: "Alice",
+  publicUrl: "http://alice.example.com",
+}
+
+export const BOB = {
+  keyPair: {
+    type: "KeyPair",
+    publicKey: {
+      type: "PublicKey",
+      key: "UExBa+Qt9BfVFCfJoBWLyzNPpGkgTPEGKLxdDQwxNhY=",
+    },
+    privateKey: {
+      type: "PrivateKey",
+      key: "0ghwEILbUNJ8rCiSFWRv4aMqZxDfzvpgQ6/Pp2K+v2g=",
+    },
+  },
+  name: "Bob",
+  publicUrl: "http://bob.example.com",
+}
+
+export const EVE = {
+  keyPair: {
+    type: "KeyPair",
+    publicKey: {
+      type: "PublicKey",
+      key: "k/IqAX11Yd+iunc+6gbTk+kmxorNXBydDV6muc21FEM=",
+    },
+    privateKey: {
+      type: "PrivateKey",
+      key: "ikS/vxtoiM8u9VUoDTROsEaKEQrbSwwySO9ZiXE8dg8=",
+    },
+  },
+  name: "Eve",
+  publicUrl: "http://eve.example.com",
+}
+
+export function temporaryIdentity() {
+  let tmpdir = tmp.dirSync({unsafeCleanup: true})
+
+  fs.writeFileSync(tmpdir.name + '/config.json', JSON.stringify(ALICE))
+
+  return {
+    path: tmpdir.name,
+    cleanup: tmpdir.removeCallback,
+  }
+
+}
+
+export function client(app) {
+  return {
+
+    get(url) {
+      return new Promise((resolve, reject) => {
+        request(app)
+          .get(url)
+          .expect(200)
+          .expect('Content-Type', /^application\/json/)
+          .end(function(err, res) {
+            if(err) return reject(err)
+            resolve(res)
+          })
+      })
+    },
+
+    post(url, body) {
+      return new Promise((resolve, reject) => {
+        request(app)
+          .post(url)
+          .send(body)
+          .expect(200)
+          .expect('Content-Type', /^application\/json/)
+          .end(function(err, res) {
+            if(err) return reject(err)
+            resolve(res)
+          })
+      })
+    },
+
+  }
+}
+
+export function message(from, to, content) {
+  return {
+    box: createBox(content, from.keyPair.privateKey, to.keyPair.publicKey),
+    from: from.publicUrl + '/profile',
+    to: to.publicUrl + '/profile',
+  }
+}
