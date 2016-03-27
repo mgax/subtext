7ebdf1b46160ad4d655a19aeea3d83787d305fa7
Alex Morega <alex@grep.ro>
2016-03-27 15:38:00 +0300

    remove obsolete broker code

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 import express from 'express'
-import broker from './src/broker.js'
 
 let app = express()
 app.use(express.static('build'))
-let server = app.listen(+(process.env.PORT || 8000))
-broker(server)
+app.listen(+(process.env.PORT || 8000))
