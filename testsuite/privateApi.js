4eee030298d16aff6c475481f75daa7b07dc1dc8
Alex Morega <alex@grep.ro>
2016-03-27 19:41:32 +0300

    refactor client wrappers

--- testsuite/privateApi.js
+++ testsuite/privateApi.js
@@ -11,7 +11,7 @@ describe('private api', function() {
     let fetchProfile = (url) => profiles[url]
     this.tmp = temporaryIdentity(ALICE)
     let server = await identityserver(this.tmp.path, fetchProfile)
-    this.app = server.privateApp
+    this.ui = client(server.privateApp)
   })
 
   after(function() {
@@ -23,25 +23,25 @@ describe('private api', function() {
     const eveUrl = 'http://eve.example.com/profile'
 
     // add peer
-    let {body: resp1} = await client(this.app).post('/peers', {
+    let {body: resp1} = await this.ui.post('/peers', {
       profile: bobUrl,
     })
     assert.isTrue(resp1.ok)
 
     // add a different peer
-    let {body: resp2} = await client(this.app).post('/peers', {
+    let {body: resp2} = await this.ui.post('/peers', {
       profile: eveUrl,
     })
     assert.isTrue(resp2.ok)
 
     // add bob again; operation should be idempotent
-    let {body: resp3} = await client(this.app).post('/peers', {
+    let {body: resp3} = await this.ui.post('/peers', {
       profile: bobUrl,
     })
     assert.isTrue(resp3.ok)
 
     // see what we have
-    let {body: resp} = await client(this.app).get('/peers')
+    let {body: resp} = await this.ui.get('/peers')
     let summary = resp.peers.map(p => ({id: p.id, url: p.url}))
     assert.deepEqual(summary, [{id: 1, url: bobUrl}, {id: 2, url: eveUrl}])
     assert.equal(resp.peers[0].profile.publicKey.key, BOB.keyPair.publicKey.key)
