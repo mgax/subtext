1de30bcb32f255fbac55b7946691c8cf0180a043
Alex Morega <alex@grep.ro>
2016-03-27 19:28:29 +0300

    add two peers

--- testsuite/privateApi.js
+++ testsuite/privateApi.js
@@ -20,14 +20,25 @@ describe('private api', function() {
 
   it('saves new peer', async function() {
     const bobUrl = 'http://bob.example.com/profile'
+    const eveUrl = 'http://eve.example.com/profile'
+
+    // add peer
     let { body: resp1 } = await client(this.app).post('/peers', {
       profile: bobUrl,
     })
     assert.isTrue(resp1.ok)
 
-    let { body: resp2 } = await client(this.app).get('/peers')
-    let bob = resp2.peers.find(p => p.url == bobUrl)
-    assert.equal(bob.profile.publicKey.key, BOB.keyPair.publicKey.key)
+    // add a different peer
+    let { body: resp2 } = await client(this.app).post('/peers', {
+      profile: eveUrl,
+    })
+    assert.isTrue(resp2.ok)
+
+    // see what we have
+    let { body: resp } = await client(this.app).get('/peers')
+    let summary = resp.peers.map(p => ({id: p.id, url: p.url}))
+    assert.deepEqual(summary, [{id: 1, url: bobUrl}, {id: 2, url: eveUrl}])
+    assert.equal(resp.peers[0].profile.publicKey.key, BOB.keyPair.publicKey.key)
   })
 
 })
