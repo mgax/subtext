c14ba697f462a281432c1c93ad5bc02b3cd420a5
Alex Morega <alex@grep.ro>
2016-04-01 04:35:01 +0300

    add peer through gui

--- src/app.js
+++ src/app.js
@@ -3,18 +3,47 @@ import { React, ReactRedux, sodium } from './vendor.js'
 import { randomKeyPair, createBox, boxId } from './messages.js'
 import {
   createStore,
+  newPeer,
 } from './store.js'
 const { Provider, connect } = ReactRedux
 
-const App = ({}) => (
+function h(callback) {
+  return function(e) {
+    e.preventDefault()
+    callback()
+  }
+}
+
+function App({peers, addPeer}) { return (
+
   <div>
+    <button
+      onClick={h(() => {
+        let url = prompt('peer url')
+        addPeer(url)
+      })}
+      >add peer</button>
+    <ul>
+      {peers.map((peer) => (
+        <li key={peer.id}>{peer.url}</li>
+      ))}
+    </ul>
   </div>
-)
+
+) }
 
 window.main = function() { (async function() {
 
   window.store = createStore()
   const socket = io.connect('/')
+
+  async function send(type, ... args) {
+    let [err, res] = await new Promise((resolve) => {
+      socket.emit(type, args, resolve)
+    })
+    if(err) throw err
+    return res
+  }
 
   const ConnectedApp = connect((state) => state, mapDispatchToProps)(App)
   window.app = ReactDOM.render((
@@ -24,6 +53,17 @@ window.main = function() { (async function() {
   ), document.querySelector('#app'))
 
   function mapDispatchToProps(dispatch) { return {
+
+    addPeer: async function(url) {
+      let peer = await send('addPeer', url)
+      dispatch(newPeer(peer))
+    },
+
   }}
+
+  let peers = await send('getPeers')
+  for(let peer of peers) {
+    store.dispatch(newPeer(peer))
+  }
 
 })().catch((e) => { console.error(e.stack) }) }
--- src/store.js
+++ src/store.js
@@ -1,9 +1,28 @@
 import { Redux } from './vendor.js'
 
-const INITIAL_STATE = {}
+const NEW_PEER = 'NEW_PEER'
+export function newPeer(peer) {
+  return {
+    type: NEW_PEER,
+    peer: peer,
+  }
+}
+
+const INITIAL_STATE = {
+  peers: [],
+}
 
 function reduce(state=INITIAL_STATE, action) {
   switch(action.type) {
+
+    case NEW_PEER:
+      if(state.peers.find((p) => p.id == action.peer.id)) {
+        return state
+      }
+      return {
+        ... state,
+        peers: [].concat(state.peers, [action.peer]),
+      }
 
     default:
       return state
