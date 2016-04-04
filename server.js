c88c1faecac396af3b3f852ccb932d21d46c7bf7
Alex Morega <alex@grep.ro>
2016-04-05 00:34:57 +0300

    fix 'server' command

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 import express from 'express'
+import identityServer from './src/identityserver.js'
 
-let app = express()
-app.use(express.static('build'))
-app.listen(+(process.env.PORT || 8000))
+async function main() {
+  let identity = await identityServer(process.argv[2])
+  let app = express()
+  app.use(identity.publicApp)
+  app.use(express.static(`${__dirname}/build`))
+  let server = app.listen(+(process.env.PORT || 8000))
+  identity.websocket(server)
+}
+
+main().catch((e) => { console.error(e.stack || e) })
