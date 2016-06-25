bd49a31bec99ec3b336b35045ef7541c37419430
Alex Morega <alex@grep.ro>
2016-06-25 18:06:33 +0300

    extract `db` as method

--- src/identityserver.js
+++ src/identityserver.js
@@ -23,32 +23,35 @@ async function defaultSend(url, envelope) {
 
 class IdentityServer {
 
+  async db(query, ...args) {
+    let conn = await new Promise((resolve, reject) => {
+      let db = new sqlite3.Database(this.identityPath + '/db.sqlite', (err) => {
+        if(err) reject(err); else resolve(db)
+      })
+    })
+    async function run(query, ...args) {
+      return await new Promise((resolve, reject) => {
+        conn.all(query, ...args, (err, rows) => {
+          if(err) reject(err); else resolve(rows)
+        })
+      })
+    }
+    let rows = await run(query, ...args)
+    rows.lastInsertId = async function() {
+      let [{id}] = await run(`SELECT last_insert_rowid() as id`)
+      return id
+    }
+    return rows
+  }
+
   async initialize(identityPath, fetchCard, send) {
+    this.identityPath = identityPath
     let config = JSON.parse(fs.readFileSync(identityPath + '/config.json'))
     let {keyPair, publicUrl, authToken} = config
     let myPublicUrl = publicUrl + '/card'
     let events = new EventEmitter()
 
-    async function db(query, ...args) {
-      let conn = await new Promise((resolve, reject) => {
-        let db = new sqlite3.Database(identityPath + '/db.sqlite', (err) => {
-          if(err) reject(err); else resolve(db)
-        })
-      })
-      async function run(query, ...args) {
-        return await new Promise((resolve, reject) => {
-          conn.all(query, ...args, (err, rows) => {
-            if(err) reject(err); else resolve(rows)
-          })
-        })
-      }
-      let rows = await run(query, ...args)
-      rows.lastInsertId = async function() {
-        let [{id}] = await run(`SELECT last_insert_rowid() as id`)
-        return id
-      }
-      return rows
-    }
+    let db = this.db.bind(this)
 
     async function getPeerByUrl(url) {
       let [row] = await db('SELECT * FROM peer WHERE url = ?', url)
@@ -110,7 +113,7 @@ class IdentityServer {
       return {ok: true}
     }
 
-    await db(`CREATE TABLE IF NOT EXISTS prop (
+    await this.db(`CREATE TABLE IF NOT EXISTS prop (
         key TEXT UNIQUE,
         value TEXT
       )`)
