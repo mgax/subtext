7059a2b09638cacd78ea8e4132794420ea2ee674
Alex Morega <alex@grep.ro>
2016-06-26 13:41:23 +0300

    show welcome forms conditionally

--- src/ui/components/Welcome.js
+++ src/ui/components/Welcome.js
@@ -3,56 +3,66 @@ import StatefulButton from './StatefulButton.js'
 export default class Welcome extends React.Component {
 
   render() {
+    let { config } = this.props
+
     return (
       <div className='container-fluid'>
 
         <h1>Welcome to subtext!</h1>
 
-        <h2>Key pair</h2>
+        {(! config.hasKeyPair) && (
+          <div>
+            <h2>Key pair</h2>
 
-        <form className='form-inline'>
+            <form className='form-inline'>
 
-          <div className='form-group'>
-            <p className='form-control-static'>
-              You don't have a key pair. Generate one?
-            </p>
-          </div>{' '}
+              <div className='form-group'>
+                <p className='form-control-static'>
+                  You don't have a key pair. Generate one?
+                </p>
+              </div>{' '}
 
-          <StatefulButton
-            className='btn btn-primary'
-            onClick={async () => {
-              await this.props.generateKeyPair()
-              return true
-            }}
-            >generate</StatefulButton>
+              <StatefulButton
+                className='btn btn-primary'
+                onClick={async () => {
+                  await this.props.generateKeyPair()
+                  return true
+                }}
+                >generate</StatefulButton>
 
-        </form>
+            </form>
+          </div>
+        )}
 
-        <h2>Public name</h2>
+        {(! config.name) && (
+          <div>
+            <h2>Public name</h2>
 
-        <form className='form-inline'>
+            <form className='form-inline'>
 
-          <div className='form-group'>
-            <label htmlFor='welcome-name'>Name</label>{' '}
-            <input
-              type='text'
-              className='form-control'
-              id='welcome-name'
-              placeholder='Jane Doe'
-              ref='name'
-              />
-          </div>{' '}
+              <div className='form-group'>
+                <label htmlFor='welcome-name'>Name</label>{' '}
+                <input
+                  type='text'
+                  className='form-control'
+                  id='welcome-name'
+                  placeholder='Jane Doe'
+                  ref='name'
+                  />
+              </div>{' '}
 
-          <StatefulButton
-            type='submit'
-            className='btn btn-primary'
-            onClick={async () => {
-              await this.props.setName(this.refs.name.value)
-              return true
-            }}
-            >Save</StatefulButton>
+              <StatefulButton
+                type='submit'
+                className='btn btn-primary'
+                onClick={async () => {
+                  await this.props.setName(this.refs.name.value)
+                  return true
+                }}
+                >Save</StatefulButton>
 
-        </form>
+            </form>
+          </div>
+        )}
 
       </div>
     )
