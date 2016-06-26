dcfaf056c1e261a32931706f58700c4b7e6b543c
Alex Morega <alex@grep.ro>
2016-06-26 14:31:21 +0300

    change the order of welcome forms: name first

--- src/ui/components/Welcome.js
+++ src/ui/components/Welcome.js
@@ -10,30 +10,6 @@ export default class Welcome extends React.Component {
 
         <h1>Welcome to subtext!</h1>
 
-        {(! config.hasKeyPair) && (
-          <div>
-            <h2>Key pair</h2>
-
-            <form className='form-inline'>
-
-              <div className='form-group'>
-                <p className='form-control-static'>
-                  You don't have a key pair. Generate one?
-                </p>
-              </div>{' '}
-
-              <StatefulButton
-                className='btn btn-primary'
-                onClick={async () => {
-                  await this.props.generateKeyPair()
-                  return true
-                }}
-                >generate</StatefulButton>
-
-            </form>
-          </div>
-        )}
-
         {(! config.name) && (
           <div>
             <h2>Public name</h2>
@@ -59,6 +35,30 @@ export default class Welcome extends React.Component {
                   return true
                 }}
                 >Save</StatefulButton>
+
+            </form>
+          </div>
+        )}
+
+        {(! config.hasKeyPair) && (
+          <div>
+            <h2>Key pair</h2>
+
+            <form className='form-inline'>
+
+              <div className='form-group'>
+                <p className='form-control-static'>
+                  You don't have a key pair. Generate one?
+                </p>
+              </div>{' '}
+
+              <StatefulButton
+                className='btn btn-primary'
+                onClick={async () => {
+                  await this.props.generateKeyPair()
+                  return true
+                }}
+                >generate</StatefulButton>
 
             </form>
           </div>
