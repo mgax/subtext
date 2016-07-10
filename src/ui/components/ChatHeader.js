9d63e86ed4552e38a551c3d728a16da27c67fa09
Alex Morega <alex@grep.ro>
2016-07-10 09:27:57 +0300

    "configure" button

--- src/ui/components/ChatHeader.js
+++ src/ui/components/ChatHeader.js
@@ -1,8 +1,20 @@
 import { h } from '../utils.js'
+import Icon from './Icon.js'
 
 export default function ChatHeader({addPeer}) {
   return (
     <div className='clearfix'>
+
+      <button
+        type='button'
+        className='btn btn-success-outline'
+        onClick={h(() => {
+          console.log('configure')
+        })}
+        >
+        <Icon name='cog' />
+      </button>
+
       <button
         className='pull-right btn btn-success-outline'
         onClick={h(() => {
@@ -10,6 +22,7 @@ export default function ChatHeader({addPeer}) {
           if(url) addPeer(url)
         })}
         >add peer</button>
+
     </div>
   )
 }
