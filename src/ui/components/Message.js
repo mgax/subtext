b35540c23661436cfe26fce5b01f476b551dec38
Alex Morega <alex@grep.ro>
2016-08-07 17:48:12 +0300

    smaller rounded corners for adjacent messages

--- src/ui/components/Message.js
+++ src/ui/components/Message.js
@@ -1,7 +1,15 @@
 import classNames from 'classnames'
 
-export default function Message({message: {me, time, message: {text}}}) {
-  let cls = classNames('message', {'message-me': me})
+export default function Message({
+      message: {me, time, message: {text}},
+      bleedUp, bleedDown,
+    }) {
+  let cls = classNames('message', {
+    'message-me': me,
+    'message-peer': ! me,
+    'message-bleedUp': bleedUp,
+    'message-bleedDown': bleedDown,
+  })
   return (
     <li className={cls}>
       <div className='time'>{moment(time).calendar()}</div>
--- src/ui/components/MessageList.js
+++ src/ui/components/MessageList.js
@@ -6,12 +6,24 @@ export default class MessageList extends React.Component {
 
   render() {
     let {className, peer} = this.props
-    let messages = sorted(Object.values(peer.messages), (m) => m.time)
+
+    let items = []
+    let prev = null
+    for (let m of sorted(Object.values(peer.messages), (m) => m.time)) {
+      let current = {message: m}
+      if(prev && prev.message.me == current.message.me) {
+        prev.bleedDown = true
+        current.bleedUp = true
+      }
+      items.push(current)
+      prev = current
+    }
+
     return (
       <ul className={classNames(className, 'messageList')}
           onScroll={h(() => { this.onScroll() })}>
-        {messages.map((message) => (
-          <Message key={message.id} message={message} />
+        {items.map((item) => (
+          <Message key={item.message.id} {... item} />
         ))}
       </ul>
     )
