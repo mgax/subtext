dd013271947c97d2839e5763e11bc62dcfcfcfcd
Alex Morega <alex@grep.ro>
2016-08-13 16:50:37 +0300

    extract inline functions as methods

--- src/ui/Ui.js
+++ src/ui/Ui.js
@@ -6,46 +6,46 @@ import App from './components/App.js'
 
 export default class Ui {
 
+  constructor() {
+    this.store = createStore()
+    this.server = new Server(this.store)
+  }
+
+  mountComponent(node, Component, props) {
+    let mapState = (state) => state
+    let mapDispatch = (dispatch) => this.server.mapDispatchToProps(dispatch)
+    let ConnectedComponent = connect(mapState, mapDispatch)(Component)
+    ReactDOM.render((
+      <Provider store={this.store}>
+        <ConnectedComponent {... props} />
+      </Provider>
+    ), node)
+  }
+
+  modal(Component, props) {
+    let node = document.querySelector('#modal')
+    this.mountComponent(node, Component, props)
+    $('.modal', node).modal().on('hidden.bs.modal', () => {
+      ReactDOM.unmountComponentAtNode(node)
+    })
+  }
+
+  updateTitle() {
+    let state = this.store.getState()
+    let peers = Object.values((state || {}).peers || [])
+    let anyUnread = any(peers.map((p) => p.unread))
+    let title = `SubText${anyUnread ? ' *' : ''}`
+    document.querySelector('head title').textContent = title
+  }
+
   mount() {
-    let store = createStore()
-    let server = new Server(store)
+    this.mountComponent(document.querySelector('#app'), App, {
+      modal: this.modal.bind(this),
+    })
 
-    function mount(node, Component, props) {
-      let mapState = (state) => state
-      let mapDispatch = (dispatch) => server.mapDispatchToProps(dispatch)
-      let ConnectedComponent = connect(mapState, mapDispatch)(Component)
-      ReactDOM.render((
-        <Provider store={store}>
-          <ConnectedComponent {... props} />
-        </Provider>
-      ), node)
-    }
+    this.store.subscribe(() => this.updateTitle())
 
-    function modal(Component, props) {
-      let node = document.querySelector('#modal')
-      mount(node, Component, props)
-      $('.modal', node).modal().on('hidden.bs.modal', () => {
-        ReactDOM.unmountComponentAtNode(node)
-      })
-    }
-
-    function updateTitle() {
-      let state = store.getState()
-      let peers = Object.values((state || {}).peers || [])
-      let anyUnread = any(peers.map((p) => p.unread))
-      let title = `SubText${anyUnread ? ' *' : ''}`
-      document.querySelector('head title').textContent = title
-    }
-
-    mount(document.querySelector('#app'), App, {modal})
-
-    store.subscribe(updateTitle)
-
-    window.S = {
-      app: app,
-      store: store,
-      server: server,
-    }
+    window.S = this
   }
 
 }
