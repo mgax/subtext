fcd8e97db5017ed6f873e079381c0e133ac54414
Alex Morega <alex@grep.ro>
2016-08-18 00:41:08 +0300

    serve customUi as ui.js

--- cmd/server.js
+++ cmd/server.js
@@ -8,20 +8,24 @@ import createServer from '../src/server/create.js'
 const BUILD = `${__dirname}/../build`
 const UI_JS = process.env.SUBTEXT_UI_JS || 'ui.js'
 
-function getUi() {
-  return fs.readFileSync(`${BUILD}/ui.js`, 'utf8')
+function getUi(customUi) {
+  let uiUrl = url.resolve(`file://${BUILD}/ui.js`, customUi)
+  let m = uiUrl.match(/^file:\/\/(.*)$/)
+  if(m) return fs.readFileSync(m[1], 'utf8')
+  throw Error(`Can't read ui.js from "${uiUrl}"`)
 }
 
 waiter((async function() {
 
   let [varPath, publicUrl, cmd] = process.argv.slice(2)
 
-  let { app } = await createServer(varPath, publicUrl)
+  let { app, server } = await createServer(varPath, publicUrl)
 
   app.get('/', function(req, res) { res.send(index()) })
   app.get('/ui.js', function(req, res) {
     res.set('Content-Type', 'application/javascript')
-    res.send(getUi())
+    let customUi = server.prop('customUi') || ''
+    res.send(getUi(customUi))
   })
   app.use(express.static(BUILD))
 
