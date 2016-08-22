48d569a2305f2ef36d746a7d64a10adf3d4661f0
Alex Morega <alex@grep.ro>
2016-08-23 02:58:19 +0300

    complain if an envvar is missing

--- src/server/create.js
+++ src/server/create.js
@@ -7,15 +7,16 @@ function bail(text) {
   process.exit(1)
 }
 
-const pleaseSetAuthToken =
-  "Please set SUBTEXT_AUTH_TOKEN. " +
-  "Make it a good passphrase."
-
 export default async function main() {
   let varPath = process.env.SUBTEXT_VAR
+  if(! varPath) bail("Please set SUBTEXT_VAR to a directory where subtext "
+      + "can save its database.")
   let publicUrl = process.env.SUBTEXT_PUBLIC_URL
+  if(! publicUrl) bail("Please set SUBTEXT_PUBLIC_URL to the URL where "
+      + "subtext will be accessible.")
   let authToken = process.env.SUBTEXT_AUTH_TOKEN
-  if(! authToken) bail(pleaseSetAuthToken)
+  if(! authToken) bail("Please set SUBTEXT_AUTH_TOKEN. Make it a good "
+      + "passphrase.")
   let server = await identityServer(varPath, publicUrl, authToken)
   let app = express()
   app.use(server.createApp())
