1502a32e8aa67a502c7e26e57ec6675493e72a47
Alex Morega <alex@grep.ro>
2016-08-23 03:03:04 +0300

    refactor reading of env vars

--- src/server/create.js
+++ src/server/create.js
@@ -2,21 +2,21 @@ import express from 'express'
 import identityServer from './identityserver.js'
 import cron from './cron.js'
 
-function bail(text) {
-  console.error(text)
+function getConfig(env, name, message) {
+  let value = env[name]
+  if(value) return value
+  console.error(`Missing environment variable ${name}: ${message}`)
   process.exit(1)
 }
 
 export default async function main() {
-  let varPath = process.env.SUBTEXT_VAR
-  if(! varPath) bail("Please set SUBTEXT_VAR to a directory where subtext "
-      + "can save its database.")
-  let publicUrl = process.env.SUBTEXT_PUBLIC_URL
-  if(! publicUrl) bail("Please set SUBTEXT_PUBLIC_URL to the URL where "
-      + "subtext will be accessible.")
-  let authToken = process.env.SUBTEXT_AUTH_TOKEN
-  if(! authToken) bail("Please set SUBTEXT_AUTH_TOKEN. Make it a good "
-      + "passphrase.")
+  let env = process.env
+  let varPath = getConfig(env, 'SUBTEXT_VAR',
+      "path to a directory where subtext can save its database")
+  let publicUrl = getConfig(env, 'SUBTEXT_PUBLIC_URL',
+      "the URL where subtext will be accessible")
+  let authToken = getConfig(env, 'SUBTEXT_AUTH_TOKEN',
+      "passphrase that will be required to log into the UI")
   let server = await identityServer(varPath, publicUrl, authToken)
   let app = express()
   app.use(server.createApp())
