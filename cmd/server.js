a316860a430dcc28c1ae68382977a5958c0b4c82
Alex Morega <alex@grep.ro>
2016-09-24 16:27:04 +0300

    run production server without babel-node

--- cmd/server.js
+++ cmd/server.js
@@ -4,11 +4,12 @@ import express from 'express'
 import waiter from '../src/waiter.js'
 import {index} from '../src/build.js'
 import createServer from '../src/server/create.js'
-
-const BUILD = `${__dirname}/../build`
+import getConfig from '../src/server/getConfig.js'
 
 function getUi(customUi) {
-  let uiUrl = url.resolve(`file://${BUILD}/ui.js`, customUi)
+  let uiJsPath = getConfig(process.env,
+    'SUBTEXT_UI_JS', "path to default ui.js")
+  let uiUrl = url.resolve(`file://${uiJsPath}`, customUi)
   let m = uiUrl.match(/^file:\/\/(.*)$/)
   console.log('loading', m[1])
   if(m) return fs.readFileSync(m[1], 'utf8')
@@ -18,8 +19,12 @@ function getUi(customUi) {
 waiter((async function() {
 
   let { app, server } = await createServer()
-  app.get('/', function(req, res) { res.send(index()) })
-  app.get('/vanilla', function(req, res) { res.send(index({vanilla: true})) })
+  app.get('/', function(req, res) {
+    res.send(index({dev: false}))
+  })
+  app.get('/vanilla', function(req, res) {
+    res.send(index({dev: false, vanilla: true}))
+  })
   app.get('/ui.js', function(req, res) {
     let customUi = server.prop('customUi') || ''
     let uiUrl = req.query.vanilla == 'y' ? '' : customUi
