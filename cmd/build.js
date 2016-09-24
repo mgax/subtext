1ad0e027f7c9033740beb52d88f72ba73f3664b7
Alex Morega <alex@grep.ro>
2016-09-24 13:38:52 +0300

    refactor build script to allow multiple targets

--- cmd/build.js
+++ cmd/build.js
@@ -3,7 +3,21 @@ import {buildUi} from '../src/build.js'
 
 waiter((async function() {
 
-  let stats = await buildUi()
-  console.log(stats.toString({colors: true}))
+  for(let name of (process.argv[2] || 'ui').split(',')) {
+    switch(name) {
+
+      case 'ui': {
+        let uiStats = await buildUi()
+        console.log(uiStats.toString({colors: true}))
+        continue
+      }
+
+      default: {
+        console.error('unknown target', name)
+        process.exit(1)
+      }
+
+    }
+  }
 
 })())
